6f9723522dfb772154c6010529ca756e5569f300
lanzhiheng <lanzhihengrj@gmail.com>
2018-06-03 20:26:48 +0800

    Add thunk promise and logger middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './index.css'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import logger from 'redux-logger'
+import promise from 'redux-promise-middleware/dist/es/index'
 import { Provider, connect } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+
+// Add thunk
+import thunk from 'redux-thunk'
 
 function reducer(state, action) {
   switch (action.type) {
@@ -10,21 +15,26 @@ function reducer(state, action) {
       return Object.assign({}, state, {counter: state.counter + 1})
     case 'DECREMENT':
       return Object.assign({}, state, {counter: state.counter - 1})
+    case 'DONE':
+      console.log(action.loginformation);
+      return state
     default:
       return state
   }
 }
 
-let logMiddle = (store) => (dispatch) => (action) => {
-  console.log('dispatch', action.type);
-  dispatch(action)
-  console.log(store.getState());
-  console.log('finish', action.type);
-}
 
-let store = applyMiddleware(logMiddle)(createStore)(reducer, {counter: 3, number: 4})
+const promiseMiddleware = promise({
+  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILD']
+})
+let store = applyMiddleware(thunk, promiseMiddleware, logger)(createStore)(reducer, {counter: 3, number: 4})
 
 class App extends Component {
+  componentDidMount() {
+    const { requestResource } = this.props
+    requestResource()
+  }
+
   render() {
     const { increment, decrement, counter } = this.props
 
@@ -43,13 +53,30 @@ const mapStateToProps = (state) => {
     counter: state.counter
   }
 }
+
+function timeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms, 'Hello World');
+  });
+}
+
+const first = () => {
+  return (dispatch) => {
+    dispatch({
+      type: 'INCREMENT',
+      payload: timeout(5000)
+    })
+  }
+}
+
 const mapDispatchToProps = dispatch => ({
   increment: () => dispatch({
     type: 'INCREMENT'
   }),
   decrement: () => dispatch({
     type: 'DECREMENT'
-  })
+  }),
+  requestResource: () => dispatch(first())
 })
 
 const NewApp = connect(mapStateToProps, mapDispatchToProps)(App)
